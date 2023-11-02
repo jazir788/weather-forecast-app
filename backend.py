eeb24715d78461dfd48fb2c28a652bf4fec25063
jazir788 <jazirsx5@gmail.com>
2023-11-02 11:56:18 +0000

    Retrieved data from openweather api in the backend

---
+++ backend.py
@@ -0,0 +1,11 @@
+import requests
+import config
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={config.API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
